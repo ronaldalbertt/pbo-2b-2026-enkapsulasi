22c24fef0d945fd246a2ac020794300c2fa41c2e
Ronald Albertt <muhammadronaldalbert@gmail.com>
2026-03-13 11:09:09 +0700

    menambahkan tugas enkapsulasi

--- book.py
+++ book.py
@@ -1,3 +1,4 @@
+import datetime
 class Book:
     def __init__(self, id_buku, judul, penulis, tahun):
         self.__id_buku = id_buku
@@ -5,3 +6,11 @@ class Book:
         self.penulis = penulis
         self.tahun = tahun
         self.status = "tersedia"
+        self.__update_terakhir = datetime.datetime.now()
+
+    def update_judul(self, judul_baru):
+        self.judul = judul_baru
+        self.__update_terakhir = datetime.datetime.now()
+
+    def get_update_terakhir(self):
+        return self.__update_terakhir
--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from book import Book
 
 buku1 = Book(1, "Belajar Python", "Affan", 2025)
+buku2 = Book(2, "Belajar JAVA", "Albert", 2026)
+buku3 = Book(3, "Belajar PBO", "Gibran", 2026)
+buku4 = Book(4, "Belajar JAVA", "Albert", 2026)
 
 print(buku1.judul)
+print(buku1.get_update_terakhir())
+
+buku1.update_judul("Belajar Python Bersama Albert")
+
+print(buku1.judul)
+print(buku1.get_update_terakhir())
+
+
+
+print(buku2.judul)
+print(buku2.get_update_terakhir())
+
+buku2.update_judul("Belajar JAVA Bersama Albert")
+
+print(buku2.judul)
+print(buku2.get_update_terakhir())
